6fa89f7b0d68c21bf005ae202f1af6a83948f6a7
smin95 <seung.min@mail.mcgill.ca>
2024-07-01 12:46:59 +0800

    edit

--- gghalves_link.R
+++
@@ -1,5 +0,0 @@
-#g <- 'http://cran.r-project.org/src/contrib/Archive/gghalves/gghalves_0.1.1.tar.gz'
-#install.packages(g, repos=NULL, type="source")
-
-g <- 'http://cran.r-project.org/src/contrib/Archive/ggplot2/ggplot2_3.3.6.tar.gz'
-install.packages(g, repos=NULL, type="source")
